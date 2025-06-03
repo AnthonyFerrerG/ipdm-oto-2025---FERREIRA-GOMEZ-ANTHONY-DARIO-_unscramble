66666ee28d179988cc6e12a2e4cdecefce3ae20a
AnthonyFerrerG <anthonydario999710@gmail.com>
2025-06-02 22:00:17 -0300

    WordsData.kt

---
+++ WordsData.kt
@@ -0,0 +1,6 @@
+package com.example.unscramble.data
+
+val allWords = listOf(
+    "kotlin", "compose", "viewmodel", "android", "state", "function", "variable",
+    "architecture", "project", "activity", "fragment", "jetpack", "studio", "layout"
+)
